217976d044ec279607b3792a96d43813dee17346
ytawakalitu48 <ytawakalitu48@gmail.com>
2026-03-26 17:02:30 +0100

    new update

--- src/component/Offer.jsx
+++ src/component/Offer.jsx
@@ -1,69 +1,82 @@
 import React from "react";
 import { GoDotFill } from "react-icons/go";
 import { MdSubdirectoryArrowRight } from "react-icons/md";
-
+import star from "../assets/star (1).png";
+import splash from "../assets/splash.png";
+import sound from "../assets/sound-waves.png";
 const Offer = () => {
+  const data = [
+    {
+      title: "Web Design",
+      subtitle:
+        " Creating visually stunning and user-friendly websites is at the heart of what I do.",
+      listone: "Custom Tailored Design",
+      listtwo: "Responsive Layouts",
+      listhree: "Custom Tailored Design",
+      icon: star,
+      green: true,
+    },
+
+    {
+      title: "Development",
+      subtitle:
+        " I bring your designs to life with clean, efficient, and high-performing code.",
+      listone: "HTML, CSS, & JS Expertise",
+      listtwo: "Cross-Browser Compatibility",
+      listhree: "Faster Load Times",
+      icon: splash,
+      color: false,
+      black: true,
+    },
+
+    {
+      title: "Brand Identity",
+      subtitle:
+        " Building a strong brand identity is essential for standing out in a crowded market.",
+      listone: "Logo Design",
+      listtwo: "Cohesive Color Palettes",
+      listhree: "Brand Guidelines",
+      icon: sound,
+      white: true,
+    },
+  ];
   return (
     <section>
-      <div className="bg-blue-300">
-        <div className=" ">
-          <div className="">
-            <p className="ring-1 ring-black flex w-fit rounded-2xl px-2">
-              <GoDotFill className="text-green-300 text-2xl" />
-              What i Offer
-            </p>
-            <h1 className="text-7xl font-medium w-[35%]">Empowering Brands Through Design</h1>
-          </div>
-          <div className="flex">
-
-          <div className="bg-lime-400 ring-2 rounded-2xl mb-6">
-            <h1 className="text-2xl font-normal ">Web Design</h1>
-            <p >
-              Creating visually stunning and user-friendly websites is at the
-              heart of what I do.
+      <div className="bg-black py-20 ">
+        <div className="mx-auto space-y-20 ">
+          <div className="mx-auto w-fit mt-20">
+            <p className="ring-1 w-fit flex rounded-2xl text-white px-4">
+              <GoDotFill className="text-green-300 text-2xl" /> WHAT I OFFER
             </p>
 
-            <p className="border-b-2">Custom Tailored Design</p>
-            <p className="border-b-2">Responsive Layouts</p>
-            <p className="border-b-2">Custom Tailored Design</p>
-
-            <button className="rounded-2xl bg-green-300 flex p-2 font-normal hover:bg-white mt-14 text-black">
-              <MdSubdirectoryArrowRight />
-              Schedule a consultation
-            </button>
+            <h1 className="font-normal text-7xl w-[70%] text-white ">
+              Empowering Brands Through Design
+            </h1>
           </div>
-          <div className="">
-            <h1>Development</h1>
-            <p>
-              I bring your designs to life with clean, efficient, and
-              high-performing code.
-            </p>
 
-            <p className="border-b-2">HTML, CSS, & JS Expertise</p>
-            <p className="border-b-2">Cross-Browser Compatibility</p>
-            <p className="border-b-2">Faster Load Times</p>
+          <div className="flex mx-auto gap-6 w-[60%]">
+            {data.map((lit, index) => (
+              <div
+                key={index}
+                className={`${lit.green && "bg-lime-400 text-black"} ${lit.white ? "bg-white text-black" : " "} ${lit.black && "text-white"} bg-black   rounded-2xl px-10 py-14 w-[400px] space-y-3 `}
+              >
+                <img className="w-14" src={lit.icon} alt="" />
+                <h1 className="font-semibold text-3xl">{lit.title}</h1>
 
-            <button className="rounded-2xl bg-green-300 flex p-2 font-normal hover:bg-white mt-14">
-              <MdSubdirectoryArrowRight />
-              Schedule a consultation
-            </button>
-          </div>
-          <div className="border-2 rounded-2xl p-10 w-fit bg-white">
-            <h1>Brand Identity</h1>
-            <p>
-              Building a strong brand identity is essential for standing out in
-              a crowded market.
-            </p>
+                <p className="mb-6">{lit.subtitle}</p>
 
-            <p className="border-b-2">Logo Design</p>
-            <p className="border-b-2">Cohesive Color Palettes</p>
-            <p className="border-b-2">Brand Guidelines</p>
+                <div className="space-y-4">
+                  <p className="border-b pb-4">{lit.listone}</p>
+                  <p className="border-b pb-4">{lit.listtwo}</p>
+                  <p className="pb-4">{lit.listhree}</p>
+                </div>
 
-            <button className="rounded-2xl bg-green-300 flex p-2 font-normal hover:bg-white mt-14">
-              <MdSubdirectoryArrowRight />
-              Schedule a consultation
-            </button>
-          </div>
+                <button className="rounded-2xl bg-green-300 flex p-2 font-normal hover:bg-white mt-14 ">
+                  <MdSubdirectoryArrowRight className="font-extralight text-2xl " />
+                  Schedule a consultation
+                </button>
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/component/Raters.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Raters = () => {
+  return (
+    <section>
+      <div>
+        <div className="ring-1 bg-gray-800 text-white rounded-2xl w-[17%] mx-auto space-y-4">
+          <p className="text-green-300">TAWAKALIT YUSUF</p>
+
+          <h1 className="font-semibold text-3xl w-[50%] pb-[35%]">
+            My Success Stories
+          </h1>
+
+          <p>
+            I take pride in collaborating with a diverse range of clients, from
+            ambitious startups to established enterprises.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Raters;
